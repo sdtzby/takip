0a45718f3be01305386ab2eb5eb6b271be0985ed
Sedat <sedattozbay@gmail.com>
2026-08-09 01:45:59 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE="beslenme-pwa-v1";
+const SHELL=["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",e=>{
+  const u=new URL(e.request.url);
+  if(e.request.method!=="GET"||u.origin!=="https://"+self.location.host)return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
